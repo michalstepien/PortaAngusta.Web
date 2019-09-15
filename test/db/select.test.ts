34788bea02ca62009abcf854e5bd04c923eef2e1
michalstepien <michal.stepien.job@gmail.com>
2019-09-15 19:06:42 +0200

    cov

--- test/db/select.test.ts
+++ test/db/select.test.ts
@@ -3,13 +3,15 @@ import { User } from '../../app/models/user';
 
 const expect = chai.expect;
 
-describe('Query from DB', () => {
-  it('Query users', () => {
-    const usrs = new User();
-    return usrs.loadAll()
-      .then((u) => {
-        expect(u[0].name).to.eql('admin');
-      });
-  });
+describe('User', () => {
+    describe('#constructor()', () => {
+        it('requires two numerical arguments', () => {
+            const usrs = new User();
+            return usrs.loadAll()
+              .then((u) => {
+                expect(u[0].name).to.eql('admin');
+              });
+          });
+    });
 
 });

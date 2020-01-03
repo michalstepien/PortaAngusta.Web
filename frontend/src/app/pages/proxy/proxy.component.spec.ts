72893af15d900e201442d72c47fa4b703306ed2e
michalstepien <michal.stepien.job@gmail.com>
2020-01-03 11:48:55 +0100

    next

--- frontend/src/app/pages/proxy/proxy.component.spec.ts
+++ frontend/src/app/pages/proxy/proxy.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { ProxyComponent } from './proxy.component';
 
 describe('ProxyComponent', () => {
@@ -8,9 +9,10 @@ describe('ProxyComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProxyComponent ]
+      imports: [MatIconModule, MatCardModule],
+      declarations: [ProxyComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

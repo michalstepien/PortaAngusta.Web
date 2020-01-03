c581b93a0019566094c5375596b937c903966497
michalstepien <michal.stepien.job@gmail.com>
2020-01-03 12:57:21 +0100

    fix

--- frontend/src/app/pages/login/login.component.spec.ts
+++ frontend/src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -9,10 +16,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
-      declarations: [ LoginComponent ]
+      imports: [RouterTestingModule, HttpClientTestingModule,
+        BrowserAnimationsModule, FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule,
+        FlexLayoutModule],
+      declarations: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- frontend/src/app/pages/proxy/proxy.component.spec.ts
+++ frontend/src/app/pages/proxy/proxy.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { ProxyComponent } from './proxy.component';
 
 describe('ProxyComponent', () => {
@@ -9,7 +18,18 @@ describe('ProxyComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatIconModule, MatCardModule],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        MatIconModule,
+        MatCardModule,
+        MatButtonModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatExpansionModule,
+        MatSelectModule,
+        MonacoEditorModule,
+        MatCheckboxModule],
       declarations: [ProxyComponent]
     })
       .compileComponents();
